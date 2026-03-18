bb1af7cc71a50be1372c80580cc098c8eaed8e81
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2026-03-18 09:32:18 +0100

    [dashpilot-android] replay=0 for real-time MutableSharedFlow

--- dashpilot-android/app/src/main/java/com/softwiredtech/dashpilot/datasource/CommaDataSource.kt
+++ dashpilot-android/app/src/main/java/com/softwiredtech/dashpilot/datasource/CommaDataSource.kt
@@ -5,6 +5,7 @@ import com.softwiredtech.dashpilot.jni.VehicleBridge
 import com.softwiredtech.dashpilot.vehicle.CanFrameDecoder
 import com.softwiredtech.dashpilot.vehicle.VehicleProfile
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.sample
@@ -13,7 +14,12 @@ class CommaDataSource(
     private val bridge: VehicleBridge,
     private val profile: VehicleProfile
 ) : IDataSource {
-    private val _incoming = MutableSharedFlow<CarState>(replay = 1)
+    private val _incoming = MutableSharedFlow<CarState>(
+        replay = 0,
+        extraBufferCapacity = 16,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+
     @OptIn(FlowPreview::class)
     // Change sample ms to throttle up or down the flow. It's set for 20 FPS for now.
     override val incomingMessages: Flow<CarState> = _incoming.sample(50)
