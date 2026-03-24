9a426d96f6aba0c285e029b19dd3be32f988f94a
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2026-03-24 14:17:34 +0100

    [dashpilot-android] Only show 'Load Rive file' in debug

--- dashpilot-android/app/src/main/java/com/softwiredtech/dashpilot/datamodel/DashboardConfig.kt
+++ dashpilot-android/app/src/main/java/com/softwiredtech/dashpilot/datamodel/DashboardConfig.kt
@@ -1,5 +1,6 @@
 package com.softwiredtech.dashpilot.datamodel
 
+import com.softwiredtech.dashpilot.BuildConfig
 import com.softwiredtech.dashpilot.R
 import com.softwiredtech.dashpilot.ui.LOCAL_ASSET_BASE_URL
 
@@ -12,28 +13,39 @@ data class DashboardConfig(
     val screenshotRes: Int = 0
 )
 
-val availableDashboards = listOf(
-    DashboardConfig(
-        name = "Expo Dashboard",
-        url = "https://dashpilot-expo.web.app",
-        type = DashboardType.WEB,
-        screenshotRes = R.drawable.preview_expo
-    ),
-    DashboardConfig(
-        name = "Vanilla Dashboard",
-        url = "${LOCAL_ASSET_BASE_URL}web-vanilla/index.html",
-        type = DashboardType.WEB,
-        screenshotRes = R.drawable.preview_vanilla
-    ),
-    DashboardConfig(
-        name = "Rive Dashboard",
-        url = "dashboard_test",
-        type = DashboardType.RIVE,
-        screenshotRes = R.drawable.preview_rive
-    ),
-    DashboardConfig(
-        name = "Load Rive File",
-        url = "",
-        type = DashboardType.DEV_RIVE
-    ),
-)
+val availableDashboards = buildList {
+    add(
+        DashboardConfig(
+            name = "Vanilla",
+            url = "${LOCAL_ASSET_BASE_URL}web-vanilla/index.html",
+            type = DashboardType.WEB,
+            screenshotRes = R.drawable.preview_vanilla
+        )
+    )
+    add(
+        DashboardConfig(
+            name = "Expo",
+            url = "https://dashpilot-expo.web.app",
+            type = DashboardType.WEB,
+            screenshotRes = R.drawable.preview_expo
+        )
+    )
+    add(
+        DashboardConfig(
+            name = "Rive",
+            url = "dashboard_test",
+            type = DashboardType.RIVE,
+            screenshotRes = R.drawable.preview_rive
+        )
+    )
+
+    if (BuildConfig.DEBUG) {
+        add(
+            DashboardConfig(
+                name = "Load Rive File",
+                url = "",
+                type = DashboardType.DEV_RIVE
+            )
+        )
+    }
+}
