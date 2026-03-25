b7f3a3bd8150f9f3a2095c1660d9a026bed0dc72
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2026-03-25 10:26:33 +0100

    [dashpilot-android] increase version code

--- dashpilot-android/app/build.gradle.kts
+++ dashpilot-android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.softwiredtech.dashpilot"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
