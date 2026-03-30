ad062051fc8448434778bf116c71e58c60285f3f
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2026-03-30 09:45:56 +0200

    [dashpilot-android] bump version code for open beta testing

--- dashpilot-android/app/build.gradle.kts
+++ dashpilot-android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.softwiredtech.dashpilot"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
