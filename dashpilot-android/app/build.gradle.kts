0a323168add3c9516db54102b4bce86fe5e87433
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2026-04-02 08:47:38 +0200

    alpha release v0.2.0 (#53)

--- dashpilot-android/app/build.gradle.kts
+++ dashpilot-android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.softwiredtech.dashpilot"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.1"
+        versionCode = 5
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
