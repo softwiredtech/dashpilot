2673721bc6366a3629e559294f875a18503d4568
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2026-08-14 16:46:26 +0200

    iOS release build number 2

--- dashpilot-android/app/build.gradle.kts
+++ dashpilot-android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.softwiredtech.dashpilot"
         minSdk = 29
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.5.0"
+        versionCode = 10
+        versionName = "0.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
