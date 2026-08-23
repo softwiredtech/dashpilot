908b49fecb2dea7bbfbda5bd1b633d7aff76a69d
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2026-08-23 13:26:46 +0200

    [mobile] Release: Play Store (v0.7.0), TestFlight 1.0(7) (#202)

--- dashpilot-android/app/build.gradle.kts
+++ dashpilot-android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.softwiredtech.dashpilot"
         minSdk = 29
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.6.0"
+        versionCode = 12
+        versionName = "0.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
