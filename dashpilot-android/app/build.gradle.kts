bd3159d1e9bf7cad821481fb58b288fafd9312ac
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2026-09-14 14:13:16 +0200

    Mobile release v0.8.0

--- dashpilot-android/app/build.gradle.kts
+++ dashpilot-android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.softwiredtech.dashpilot"
         minSdk = 29
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.7.0"
+        versionCode = 13
+        versionName = "0.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
