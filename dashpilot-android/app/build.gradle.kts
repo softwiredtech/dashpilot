02e8048570f60468da603b07ec1b28d8191e7144
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2026-09-16 10:13:19 +0200

    [android][ios] 0.8.1 hotfix release

--- dashpilot-android/app/build.gradle.kts
+++ dashpilot-android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.softwiredtech.dashpilot"
         minSdk = 29
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.8.0"
+        versionCode = 15
+        versionName = "0.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
